468b4f94402486a9b730a712c9cca7dd3f7a652c
Cicciocotroneo <74688471+Cicciocotroneo@users.noreply.github.com>
2025-04-15 18:17:35 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+// Nome della cache
+const CACHE_NAME = 'fountain-converter-cache-v1';
+
+// File da mettere in cache
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
+];
+
+// Installa il service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Strategia di cache: Network first, poi cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
